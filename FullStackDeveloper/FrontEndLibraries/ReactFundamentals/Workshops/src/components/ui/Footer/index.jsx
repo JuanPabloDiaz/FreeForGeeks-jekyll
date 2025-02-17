277200acbceff457c4cf01da54739b7b033bfef2
JuanPabloDiaz <juanchodis@hotmail.com>
2025-02-17 18:56:32 -0500

    testing theme: jekyll-theme-architect

---
+++ FullStackDeveloper/FrontEndLibraries/ReactFundamentals/Workshops/src/components/ui/Footer/index.jsx
@@ -0,0 +1,20 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  const startYear = 2024;
+  const currentYear = new Date().getFullYear();
+  const yearText =
+    startYear === currentYear ? startYear : `${startYear} - ${currentYear}`;
+
+  return (
+    <footer>
+      <p>© {yearText}</p>
+      <p>
+        Developed by
+        <a className="footer-link" href="https://www.linkedin.com/in/1diazdev/">
+          Juan Diaz
+        </a>
+      </p>
+    </footer>
+  );
+};
---
+++ jekyll-themes.md
@@ -0,0 +1,6 @@
+# My Favorite Jekyll Themes
+
+- [Cayman](https://pages-themes.github.io/cayman/)
+  > `theme: jekyll-theme-cayman`
+- [Architect](https://pages-themes.github.io/architect/)
+  > `theme: jekyll-theme-architect`
